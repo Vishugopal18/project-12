aad93778408ed8b23185eadd0cd2313d067f586b
Vishvashini G <129277838+Vishugopal18@users.noreply.github.com>
2023-07-11 23:08:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// function paragraph(){
+//     document.getElementById("demo").innerHTML="vishu";
+// }
+// function paragraph(){
+//     document.getElementById("demo").style.fontSize="50px";
+// }
+// function paragraph(){
+//     document.getElementById("demo").style.display="visible";
+// }
+// document.write(5+6);
+// alert(5+6);
+// console.log(5+6);
+
+// let a=5;
+// var b=6;
+// const c=7;
+// //adding to numbers
+// var x = 5;
+// var y = 5;
+// var z = x + y;
+// console.log(z);
+function add(){
+    var x = parseInt(document.getElementById("n1").value);
+    var y = parseInt(document.getElementById("n2").value);
+    var z = x + y;
+    
+    document.getElementById("add").innerHTML = z;
+    }
+function sub(){
+    var x = document.getElementById("n1").value;
+    var y = document.getElementById("n2").value;
+    var z = x - y;
+    document.getElementById("sub").innerHTML = z;
+    } 
+function mul(){
+    var x = (document.getElementById("n1").value);
+    var y = (document.getElementById("n2").value);
+    var z = x * y;
+    document.getElementById("mul").innerHTML = z;
+    }
+function div(){
+    var x = (document.getElementById("n1").value);
+    var y = (document.getElementById("n2").value);
+    var z = x / y;
+    document.getElementById("div").innerHTML = z;
+    }
+  
+// var x = 9;
+// var y = 3;
+// var z = x - y;
+// document.write(z);
+// var x = 9;
+// var y = 3;
+// var z = x * y;
+// document.write(z);
+// var x = 9;
+// var y = 3;
+// var z = x / y;
+// document.write(z);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
